e22994fd4547db7899e8f7dcec87b333c4330617
colson1111 <colson1111@gmail.com>
2015-10-20 12:25:12 -0400

    Update caret-vignette.r
    finished vignette

--- caret/caret-vignette.r
+++ caret/caret-vignette.r
@@ -1,5 +1,4 @@
 #  Learning the caret package
-# https://cran.r-project.org/web/packages/caret/vignettes/caret.pdf
 
 # ensure caret and all its dependencies are installed.  They will be loaded as needed.
 # install.packages("caret", dependencies = c("Depends", "Suggests"))
@@ -8,7 +7,7 @@ library(caret)
 library(mlbench)
 
 data(Sonar)
-set.seed(107)
+set.seed(802)
 
 inTrain <- createDataPartition(y = Sonar$Class,  # The outcome data
                                p = .75,          # The percentage of data in the training set
@@ -47,3 +46,47 @@ plsFit <- train(Class ~ .,
                 preProc = c("center", "scale") # center and scale the predictors for the training set and all future samples
 )
 plsFit
+
+# plot ROC by # of pls components
+plot(plsFit)
+
+# Predict new samples
+plsClasses <- predict(plsFit, newdata = testing)
+str(plsClasses)
+
+plsProbs <- predict(plsFit, newdata = testing, type = "prob")
+head(plsProbs)
+
+# Confusion Matrix
+confusionMatrix(data = plsClasses, testing$Class)
+
+# Fit a different model to the data (regularized discriminant model)
+rdaGrid <- data.frame(gamma = (0:4) / 4, lambda = 3/4)
+set.seed(802)
+rdaFit <- train(Class ~ .,
+                data = training,
+                method = "rda",
+                tuneGrid = rdaGrid,
+                trControl = ctrl,
+                metric = "ROC")
+rdaFit
+
+# Predict new data and view confusion matrix
+rdaClasses <- predict(rdaFit, newdata = testing)
+confusionMatrix(rdaClasses, testing$Class)
+
+
+# Use resamples function to collect, summarize, and contrast resampling results
+# make sure all set.seed() functions match above so the folds are the same
+resamps <- resamples(list(pls = plsFit, rda = rdaFit))
+summary(resamps)
+
+# plot to visualize results
+xyplot(resamps, what = "BlandAltman")
+
+# paired t-test to assess whether there is a difference in the avg. resampled AUROC
+diffs <- diff(resamps)
+summary(diffs)
+
+# difference between the models is -0.028 ROC units (RDA is higher).
+# two-sided p-value is 0.2812, suggesting no significant difference between the two models.
